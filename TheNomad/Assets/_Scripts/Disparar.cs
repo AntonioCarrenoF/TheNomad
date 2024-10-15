8d8906750ef354b0fff647069e0f7807fb981863
AntonioCarrenoF <antonio234@icloud.com>
2024-10-14 20:16:48 -0600

    Script Disparar
    Se creó la lógica para disparar el arma principal

---
+++ TheNomad/Assets/_Scripts/Disparar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{
+    public float velocidadDisparo = 10f;
+    public float distanciaDisparo = 50f;
+    public GameObject prefabBala;
+    private Camera camaraDisparo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camaraDisparo = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DispararArma();
+        }
+        
+    }
+
+    public void DispararArma()
+    {
+        GameObject tempBala = Instantiate(prefabBala, camaraDisparo.transform.position, camaraDisparo.transform.rotation);
+        tempBala.GetComponent<Rigidbody>().velocity = camaraDisparo.transform.forward * velocidadDisparo;
+        Destroy(tempBala, distanciaDisparo / velocidadDisparo);
+    }
+}
