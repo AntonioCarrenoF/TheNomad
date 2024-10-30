76835405f26ca494e83bbe42ef453fa99842a465
AntonioCarrenoF <antonio234@icloud.com>
2024-10-29 19:56:19 -0600

    Modificación Parar Tiempo
    Se modificó el script PararTiempo para no poder mover la camara al estar pausado el juego

--- TheNomad/Assets/_Scripts/PararTiempo.cs
+++ TheNomad/Assets/_Scripts/PararTiempo.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class TiempoParado : MonoBehaviour
 {
+    public Camera camara;
+    private Vector3 posicion;
+    private Quaternion rotacion;
+
     private void OnEnable()
     {
         Time.timeScale = 0.0f;
+        posicion = camara.transform.position;
+        rotacion = camara.transform.rotation;
     }
+
+    private void Update()
+    {
+        camara.transform.position = posicion;
+        camara.transform.rotation = rotacion;
+    }
+
     private void OnDisable()
     {
         Time.timeScale = 1.0f;
